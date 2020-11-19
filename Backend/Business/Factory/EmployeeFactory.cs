5b92386f94ec843d66f149751e1ea32d5e0d3d2d
Andres Zapata <andres.zapata@doctus.com.co>
2020-11-19 17:58:42 -0500

    Se organiza FactoryEmployee

--- Backend/Business/Factory/EmployeeFactory.cs
+++ Backend/Business/Factory/EmployeeFactory.cs
@@ -6,17 +6,17 @@
     {
         public static EmployeeDTO CreateEmployee(Employee employee)
         {
-            if (employee != null)
+            if (employee == null)
             {
-                if (employee.ContractTypeName == GlobalSettings.HourlyContract)
-                {
-                    return EmployeeDTO.NewHourlyEmployee(employee.Id, employee.Name, employee.ContractTypeName, employee.RoleName, employee.HourlySalary);
-                }
-
-                return EmployeeDTO.NewMonthlyEmployee(employee.Id, employee.Name, employee.ContractTypeName, employee.RoleName, employee.MonthlySalary);
+                return null;
             }
 
-            return null;
+            if (employee.ContractTypeName == GlobalSettings.HourlyContract)
+            {
+                return EmployeeDTO.NewHourlyEmployee(employee.Id, employee.Name, employee.ContractTypeName, employee.RoleName, employee.HourlySalary);
+            }
+
+            return EmployeeDTO.NewMonthlyEmployee(employee.Id, employee.Name, employee.ContractTypeName, employee.RoleName, employee.MonthlySalary);
         }
     }
 }
